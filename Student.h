0dec979493964a158093d19fa744502b917cbb6d
Yassin-amr-16001061 <yassin.hegazy@student.giu-uni.de>
2025-11-11 09:15:11 +0200

    Implement Student class extending Person
    Added Student class with member variables and methods.

--- Student.h
+++ Student.h
@@ -1,11 +1,31 @@
 #ifndef STUDENT_H
 #define STUDENT_H
-
+#include <iostream>
 #include "Person.h"
 #include <string>
 #using namespace std;
 //#write student class here
+class Student : public Person
+{   private:
+    int yearLevel;
+    std:: string major;
+public:
 
+    void setmajor(std:: string major);
+    string getmajor();
+    void setYearLevel(int YL);
+    int getYearLevel();
+
+
+    void display();
+    Student()=default;
+
+    Student(std:: string major,int yearLevel);
+    ~Student();
+};
+
+
+#endif 
 
 
 
