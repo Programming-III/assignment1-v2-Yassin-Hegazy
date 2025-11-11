5a64a76fc3d9661b075cfeacff38332355afe1a5
Yassin-amr-16001061 <yassin.hegazy@student.giu-uni.de>
2025-11-11 09:48:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,38 @@ using namespace std;
 
 // ==================== Person Class Implementation =========================
 
-
+#include "Person.h"
+Person::Person(std::string name,int id)
+{
+this->name=name;
+this->id=id;    
+}
+string Person:: getname()
+{
+return name;
+    
+}
+void  Person:: setname(std:: string n)
+{
+n=name;
+    
+}
+void  Person ::setid(int ID){
+id=ID;
+    
+};
+int Person::getid()
+{
+return id;
+    
+};
+void Person::display()
+{
+    cout<<"name ="<<name<<"id is "<<id;
+}
+Person::~Person()
+{
+}
 
 
 
@@ -18,7 +49,39 @@ using namespace std;
 
 
 // ==================== Student Class Implementation ====================
+Student::Student(string major,int yearLevel)
+{
+this->major=major;
+ this->yearLevel=yearLevel;   
 
+    
+}
+void Student:: setmajor(std:: string major)
+{
+major=major;
+ 
+};
+string Student ::getmajor()
+{
+return major;
+ 
+};
+void Student ::setYearLevel(int YL)
+{
+yearLevel=YL;
+ 
+};
+int  Student ::getYearLevel()
+{
+return yearLevel;
+ 
+};
+Student::~Student(){};
+void Student::display()
+{
+cout<<"student name is "<< getname()<< "major is "<< major<<"His yaer level is "<< yearLevel<<"his id is "<<getid();
+ 
+}
 
 
 
@@ -27,7 +90,36 @@ using namespace std;
 // ==================== Instructor Class Implementation ====================
 
 
+instructor::instructor(std:: string department ,int experienceYears )
+{
+this->experienceYears=experienceYears;
+this->department =department;
 
+};
+void instructor::setXP(int XP)
+{
+    experienceYears=XP;
+};
+
+
+int instructor:: getXP()
+{
+    return experienceYears;
+};
+void instructor:: setdepartment(std:: string department)
+{
+department=department;
+
+};
+string  instructor::getdepartment()
+{
+    return department;
+};
+void instructor::display()
+{
+    cout<<"this is the instructors departmet "<<department<<"This is his expreince years"<<experienceYears<<"instructor name is "<< getname()<<"his id is "<<getid();
+}
+instructor::~instructor(){};
 
 
 
