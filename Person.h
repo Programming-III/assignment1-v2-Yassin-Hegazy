57b2998780fe1ebb8f6dca411c3269680286bf26
Yassin-amr-16001061 <yassin.hegazy@student.giu-uni.de>
2025-11-11 09:14:23 +0200

    Implement Person class with basic functionality
    Added Person class with private members and public methods for setting and getting name and id.

--- Person.h
+++ Person.h
@@ -1,11 +1,32 @@
 #ifndef PERSON_H
 #define PERSON_H
-
+#include <iostream>
 #include <string>
 using namespace std;
 
 //#write Person class here
+class Person
+{
+private:
+   std:: string name;
+    int id;
+public:
+    void setname(std:: string name);
+    string getname();
+    void setid(int id);
+    int getid();
 
+
+    Person()=default;
+    Person(std:: string name, int id);
+    ~Person();
+    void display();
+
+
+};
+
+
+#endif 
 
 
 
