76f14c4f64a3f419527ed04f9d7e2ac44c945c46
Yassin-amr-16001061 <yassin.hegazy@student.giu-uni.de>
2025-11-11 09:12:55 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -1,6 +1,6 @@
 #ifndef INSTRUCTOR_H
 #define INSTRUCTOR_H
-
+#include <iostream>
 #include "Person.h"
 using namespace std;
 //#write Instructor class here
@@ -8,7 +8,24 @@ using namespace std;
 
 
 
+class instructor:public Person
+{
+std:: string department;
+ int experienceYears;
+public:
+ void setXP(int XP);
+ int getXP();
+ void setdepartment(std:: string department);
+ string getdepartment();
 
+ instructor()=default;
+ instructor(std:: string department ,int experienceYears );
+ ~instructor();
+void display();
+};
+
+
+#endif 
 
 
 
