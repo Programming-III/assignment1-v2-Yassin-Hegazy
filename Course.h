6dac8727cd7d28f356d8544492ccdafa53e1cfac
Yassin-amr-16001061 <yassin.hegazy@student.giu-uni.de>
2025-11-11 09:24:31 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -1,6 +1,6 @@
 #ifndef COURSE_H
 #define COURSE_H
-
+#include "Student.h"
 #include <string>
 #include "Student.h"
 using namespace std;
@@ -10,6 +10,34 @@ using namespace std;
 
 
 
+class Course
+{
+private:
+string Courscode;
+   string Coursename;
+   int maxStudents;
+   Student *student;
+   int CurrentStudents;
+public:
+    Course()=default;
+    Course(string Courscode,string Coursename,int maxStudents,Student *student, int CurrentStudents);
+    ~Course();
+addStudent(const Student & s);
+void Displaycourseinfo();
+  string getCoursecode();
+    string getCoursename();
+    int getmaxstudents();
+    int getCurrentStudents();
+    void setCoursecoode(string Coursecode);
+    void setCoursename(string Coursename);
+    void setmaxstudents(int maxstudents);
+    void setCurrentstudents(int Currentstudents);
+
+
+};
+
+
+#endif 
 
 
 
